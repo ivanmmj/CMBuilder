b6ddde055f503f72880bcf44f3bf81d9c33e7a49
Ivan Maldonado <you@example.com>
2011-03-28 16:56:33 -0400

    Added all available devices.

--- scripts/devicesetup.sh
+++ scripts/devicesetup.sh
@@ -54,6 +54,206 @@ cyan_crespo() {
 	. build/envsetup.sh
 	lunch 55
 	echo lunch=55  > ../scripts/device
-	extract=device/bn/encore
+	extract=device/samsung/crespo
+	startextract
+}
+
+cyan_desirec() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_desirec.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 56
+	echo lunch=56  > ../scripts/device
+	extract=device/htc/desirec
+	startextract
+}
+
+cyan_ds() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_dream_sapphire.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 57
+	echo lunch=57  > ../scripts/device
+	extract=device/htc/dream_sapphire
+	startextract
+}
+
+cyan_espresso() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_espresso.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 58
+	echo lunch=58  > ../scripts/device
+	extract=device/htc/espresso
+	startextract
+}
+
+cyan_glacier() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_glacier.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 60
+	echo lunch=60  > ../scripts/device
+	extract=device/htc/glacier
+	startextract
+}
+
+cyan_harmony() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_harmony.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 61
+	echo lunch=61  > ../scripts/device
+	extract=device/nvidia/harmony
+	startextract
+}
+
+cyan_hero() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_hero.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 62
+	echo lunch=62  > ../scripts/device
+	extract=device/htc/hero
+	startextract
+}
+
+cyan_heroc() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_heroc.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 63
+	echo lunch=63  > ../scripts/device
+	extract=device/htc/heroc
+	startextract
+}
+
+cyan_inc() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_inc.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 64
+	echo lunch=64  > ../scripts/device
+	extract=device/htc/inc
+	startextract
+}
+
+cyan_legend() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_legend.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 65
+	echo lunch=65  > ../scripts/device
+	extract=device/htc/legend
+	startextract
+}
+
+cyan_leo() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_leo.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 66
+	echo lunch=66  > ../scripts/device
+	extract=device/htc/leo
+	startextract
+}
+
+cyan_liberty() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_liberty.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 67
+	echo lunch=67  > ../scripts/device
+	extract=device/htc/liberty
+	startextract
+}
+
+cyan_one() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_one.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 68
+	echo lunch=68  > ../scripts/device
+	extract=device/geeksphone/one
+	startextract
+}
+
+cyan_passion() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_passion.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 69
+	echo lunch=69  > ../scripts/device
+	extract=device/htc/passion
+	startextract
+}
+
+cyan_sholes() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_sholes.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 70
+	echo lunch=70  > ../scripts/device
+	extract=device/motorola/sholes
+	startextract
+}
+
+cyan_speedy() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_speedy.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 71
+	echo lunch=71  > ../scripts/device
+	extract=device/htc/speedy
+	startextract
+}
+
+cyan_supersonic() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_supersonic.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 72
+	echo lunch=72  > ../scripts/device
+	extract=device/htc/supersonic
+	startextract
+}
+
+cyan_vega() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_vega.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 73
+	echo lunch=73  > ../scripts/device
+	extract=device/advent/vega
+	startextract
+}
+
+cyan_vision() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_vision.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 74
+	echo lunch=74  > ../scripts/device
+	extract=device/htc/vision
+	startextract
+}
+
+cyan_z71() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_z71.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 75
+	echo lunch=75  > ../scripts/device
+	extract=device/commtiva/z71
+	startextract
+}
+
+cyan_zero() {
+	SoDir
+	cp ./vendor/cyanogen/products/cyanogen_z71.mk ./buildspec.mk	
+	. build/envsetup.sh
+	lunch 76
+	echo lunch=76  > ../scripts/device
+	extract=device/geeksphone/zero
 	startextract
 }
